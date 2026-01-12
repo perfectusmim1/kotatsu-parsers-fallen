52349a235968502f24d33dbec15aa69be0dbdf46
perfectusmim1 <perfectusmim@gmail.com>
2026-01-12 09:46:22 +0300

    ff

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Siyahmelek.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Siyahmelek.kt
@@ -1,16 +1,13 @@
 package org.koitharu.kotatsu.parsers.site.madara.tr
 
-import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
-@Broken
-//This source requires an account.
-@MangaSourceParser("GRIMELEK", "Grimelek", "tr")
-internal class Grimelek(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.GRIMELEK, "siyahmelek.org", 20) {
+@MangaSourceParser("GRIMELEK", "Siyahmelek", "tr")
+internal class Siyahmelek(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.GRIMELEK, "siyahmelek.lol", 20) {
 	override val datePattern = "d MMMM yyyy"
-	override val listUrl = "seri/"
+	override val listUrl = "manga/"
 }
